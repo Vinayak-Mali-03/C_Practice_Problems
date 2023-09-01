86cb0abc2f13869289e1fbb714069fbbd2559e32
Vinayak-Mali-03 <vinayakm0505@gmail.com>
2023-09-01 23:57:12 +0530

    Initial commit

---
+++ 001_Hello_World.c
@@ -0,0 +1,20 @@
+/**
+ * @file 001_Hello_World.c
+ * @author Vinayak Mali
+ * @brief To print hellow world message
+ * @version 0.1
+ * @date 2023-09-01
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    printf("Hello World...!\n");
+
+    exit(EXIT_SUCCESS);
+}
