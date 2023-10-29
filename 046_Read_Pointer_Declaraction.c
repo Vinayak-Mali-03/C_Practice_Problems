ad3339ddf9ea8e206d6e6a183e132e7d24dfa44a
Vinayak-Mali-03 <vinayakm0505@gmail.com>
2023-10-29 23:42:27 +0530

    Initial commit

---
+++ 046_Read_Pointer_Declaraction.c
@@ -0,0 +1,46 @@
+/**
+ * @file 046_Read_Pointer_Declaraction.c
+ * @author Vinayak Mali
+ * @brief To demonstrate how to read pointer declaractions
+ * @version 0.1
+ * @date 2023-10-29
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    /*
+        int* ptr - ptr is pointer to int.
+
+        int const* ptr - ptr is pointer to constant int
+
+        int* const ptr - ptr is constant pointer to int
+
+        const int* const ptr - ptr is constant pointer to const int
+
+        ------------------------------------------------------
+    
+        const int* ptr     equal to     int const* ptr
+
+        const int * const ptr     equal to      int const* const ptr
+
+
+        ------------------------------------------------------ 
+
+
+        int** ptr - ptr is pointer to pointer ot int
+
+        int** const ptr - ptr is const pointer to pointer to int
+
+        int * cosnt * ptr - ptr is pointer to a const pointer to an int
+
+        int const ** ptr - ptr is a pointer to a pointer to a const int
+
+        int * const * const ptr - ptr a const pointer to const pointer to int      
+    */
+}
