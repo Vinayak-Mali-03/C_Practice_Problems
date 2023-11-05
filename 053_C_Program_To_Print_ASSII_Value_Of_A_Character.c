b2a81613166b98c35824d75ea755fc4f91a88a6b
Vinayak-Mali-03 <vinayakm0505@gmail.com>
2023-11-05 23:42:14 +0530

    Initial commit

---
+++ 053_C_Program_To_Print_ASSII_Value_Of_A_Character.c
@@ -0,0 +1,22 @@
+/**
+ * @file 053_C_Program_To_Print_ASSII_Value_Of_A_Character.c
+ * @author Vinayak Mali
+ * @brief To print the ASCII value of a character
+ * @version 0.1
+ * @date 2023-11-05
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    char c = 'k';
+
+    printf("The ASCII value of %c is %d\n", c, c);
+
+    return (EXIT_SUCCESS);
+}
