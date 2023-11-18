dea0ffdc58cdd0d23e4bea0be7a8c56d0d16887a
Vinayak-Mali-03 <vinayakm0505@gmail.com>
2023-11-18 23:50:26 +0530

    Initial commit

---
+++ 076_Triangle_Number_Pattern_4.c
@@ -0,0 +1,10 @@
+/**
+ * @file 076_Triangle_Number_Pattern_4.c
+ * @author Vinayak Mali
+ * @brief To print the triangle pattern
+ * @version 0.1
+ * @date 2023-11-18
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
