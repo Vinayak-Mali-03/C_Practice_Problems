0d478ea9bb39e08f2e88b1f4d9c25aaa3972945b
Vinayak-Mali-03 <vinayakm0505@gmail.com>
2023-11-18 23:49:56 +0530

    Initial commit

---
+++ 075_Triangle_Number_Pattern_3.c
@@ -0,0 +1,20 @@
+/**
+ * @file 075_Triangle_Number_Pattern_3.c
+ * @author Vinayak Mali
+ * @brief Print the triangle pattern 
+ * @version 0.1
+ * @date 2023-11-18
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int num, i, j;
+
+    
+}
