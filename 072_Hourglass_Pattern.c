9ee6f8aa84ea8265b19a372c365f5601c49a8d6d
Vinayak-Mali-03 <vinayakm0505@gmail.com>
2023-11-20 23:13:04 +0530

    Initial commit

--- 072_Hourglass_Pattern.c
+++ 072_Hourglass_Pattern.c
@@ -9,3 +9,32 @@
  * 
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int num, i, j;
+    int tmp;
+
+    printf("Enter the number of rows : ");
+    scanf("%d", &num);
+
+    for(i = 0; i < 2 * num - 1; ++i)
+    {
+        if(i < num)
+            tmp = 2 * i + 1;
+        else    
+            tmp = 2 * (2 * num - i) - 3;
+
+        for(j = 0; j < tmp; ++j)
+            printf(" ");
+        
+        for(j = 0; j < 2 * num - tmp; ++j)
+            printf("* ");
+
+        printf("\n");
+    }
+
+    return (EXIT_SUCCESS);
+}
