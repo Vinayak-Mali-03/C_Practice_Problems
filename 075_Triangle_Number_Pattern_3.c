da7ad484ed754785911630f33c5afc5c35d5f19b
Vinayak-Mali-03 <vinayakm0505@gmail.com>
2023-11-20 23:13:37 +0530

    Initial commit

--- 075_Triangle_Number_Pattern_3.c
+++ 075_Triangle_Number_Pattern_3.c
@@ -16,5 +16,22 @@ int main(void)
 {
     int num, i, j;
 
-    
+    printf("Enter the number of rows : ");
+    scanf("%d", &num);
+
+    for(i = 0; i < num; ++i)
+    {
+        for(j = 0; j < 2 * (num - i) - 1; ++j)
+            printf(" ");
+
+        for(j = 0; j < 2 * i + 1; ++j)
+            if(j == 0 || j == 2 * i || i == num - 1)
+                printf("%d ", j + 1);
+            else
+                printf("  ");
+
+        printf("\n");
+    }
+
+    return (EXIT_SUCCESS);
 }
